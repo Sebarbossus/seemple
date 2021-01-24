7376442009e7ac99f90cd57acc1f3c84860533d3
Nico Alvarez <nicoalvareza22@gmail.com>
2021-01-24 22:02:14 +0100

    Added first version of the app

---
+++ precache-manifest.3eb33c06882093d833eab3a5ca2a5de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb5d1afa0a337b96416552fc89ae4fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d4223a849d106379c9",
+    "url": "/static/css/main.7d1885a2.chunk.css"
+  },
+  {
+    "revision": "61a3953618b2bcb8e05e",
+    "url": "/static/js/2.6a3f73f1.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/static/js/2.6a3f73f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d4223a849d106379c9",
+    "url": "/static/js/main.5748e1ed.chunk.js"
+  },
+  {
+    "revision": "46976c2c4ede7b9bcf75",
+    "url": "/static/js/runtime-main.a5584f08.js"
+  }
+]);
